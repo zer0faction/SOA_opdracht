5d27a112a0350ead2737cb69008e5eacf6c3177d
Erin Wallaard <erin.wallaard@opensesameict.nl>
2019-02-25 12:03:51 +0100

    first

---
+++ src/Backlog.java
@@ -0,0 +1,5 @@
+import java.util.ArrayList;
+
+public class Backlog {
+    private ArrayList<BacklogItem> backlogItems;
+}
---
+++ src/BacklogItem.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+
+public class BacklogItem {
+    private String TaskDescription;
+    private enum Phase {};
+    private ArrayList<Activity> activities;
+    private Developer developer;
+}
